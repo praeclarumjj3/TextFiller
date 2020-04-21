c3bf662d21dee992e1cfcd76ab33daf035706569
praeclarumjj3 <jitesh.jj2@gmail.com>
2020-04-21 14:45:42 +0530

    Library implemented

--- app/src/main/java/com/example/textfiller/MainActivity.java
+++ app/src/main/java/com/example/textfiller/MainActivity.java
@@ -17,6 +17,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.example.textfill.TextFill;
 import com.example.textfiller.R;
 
 import static android.hardware.Sensor.TYPE_ORIENTATION;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Bitmap results,maskBitmap;
     public static int x;
     public static int y;
+    TextFill textFill;
 
     private SensorManager sensorManager = null;
 
@@ -35,7 +37,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         img = findViewById(R.id.mask);
-        Bitmap finalmasking = MaskingProcess("JJ ROCKS");
+        textFill = new TextFill();
+        Bitmap univ = BitmapFactory.decodeResource(getResources(), R.drawable.univ);
+        Bitmap finalmasking = textFill.MaskingProcess("JJ ROCKS", univ);
         img.setImageBitmap(finalmasking);
 
     }
@@ -48,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 // the values you were calculating originally here were over 10000!
                 x = (int) Math.pow(sensorEvent.values[0], 1.5);
                 y = (int) Math.pow(sensorEvent.values[2], 1.5);
-                rotateImage((float) (y*0.1/(x+0.1)));
+                textFill.rotateImage((float) (y*0.05/(x+0.1)),img);
 
             }
 
@@ -56,12 +60,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-    private void rotateImage(float rotate) {
-        Matrix matrix = new Matrix();
-        img.setScaleType(ImageView.ScaleType.MATRIX); //required
-        matrix.postRotate(rotate, img.getDrawable().getBounds().width()/2,    img.getDrawable().getBounds().height()/2);
-        img.setImageMatrix(matrix);
-    }
 
     // I've chosen to not implement this method
     public void onAccuracyChanged(Sensor arg0, int arg1)
@@ -89,53 +87,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.unregisterListener(this);
         super.onStop();
     }
-
-
-    private Bitmap MaskingProcess(String string) {
-        try{
-            Bitmap original,mask;
-            original = BitmapFactory.decodeResource(getResources(), R.drawable.univ);
-            mask = textAsBitmap(string,1000, Color.WHITE);
-
-            if(original!=null){
-                int w = original.getWidth();
-                int h = original.getHeight();
-
-                results = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
-                maskBitmap = Bitmap.createScaledBitmap(mask,w,h,true);
-
-                Canvas canvas = new Canvas(results);
-
-                Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-                paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-
-                canvas.drawBitmap(original,0,0,null);
-                canvas.drawBitmap(maskBitmap,0,0,paint);
-
-                paint.setXfermode(null);
-                paint.setStyle(Paint.Style.STROKE);
-            }
-        }catch (OutOfMemoryError outOfMemoryError){
-            outOfMemoryError.printStackTrace();
-        }
-        img.setImageBitmap(results);
-        return results;
-    }
-
-
-    public Bitmap textAsBitmap(String text, float textSize, int textColor) {
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setTextSize(textSize);
-        paint.setColor(textColor);
-        paint.setTextAlign(Paint.Align.LEFT);
-        float baseline = -paint.ascent(); // ascent() is negative
-        int width = (int) (paint.measureText(text) + 0.5f); // round
-        int height = (int) (baseline + paint.descent() + 0.5f);
-        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(image);
-        canvas.drawText(text, 0, baseline, paint);
-        return image;
-    }
-
 
 }
